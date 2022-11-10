1657d14ea175fd148884e3fcb9a5c728e335623a
jhorman9 <jhosmita9@gmail.com>
2022-11-10 18:21:20 -0500

    actualizando

--- src/component/ToDoList.jsx
+++ src/component/ToDoList.jsx
@@ -9,18 +9,20 @@ const ToDoList = ({toDoList, deleteToDo, selectToDo, toDoSelected}) => {
             {
                 toDoList.map(toDo => (
                     <li key={toDo.id}>
-                        <h2 className='title'>{toDo.title}</h2>
-                        <div className='description'>{toDo.description}</div>
-                        <div className='isAvailable'><b>Is available:</b> {toDo.isCompleted?.toString()}</div>
-                        <div className='btn'>
-                            <button className='delete' onClick={() => deleteToDo(toDo.id)}>Delete</button>
-                            <button className='select' onClick={() => selectToDo(toDo)}>Edit</button>
-                            {
-                                toDoSelected && (
-                                    <button type='button' onClick={() => selectToDo(null)}>cancel</button>
-                                )
-                            }
-                        </div>
+                        <ul>
+                            <li><h2 className='title'>{toDo.title}</h2></li>
+                            <li><p className='description'>{toDo.description}</p></li>
+                            <li><p className='isAvailable'><b>Is available:</b> {toDo.isCompleted?.toString()}</p></li>
+                            <li><div className='btn'>
+                                <button className='delete' onClick={() => deleteToDo(toDo.id)}>Delete</button>
+                                <button className='select' onClick={() => selectToDo(toDo)}>Edit</button>
+                                {
+                                    toDoSelected && (
+                                        <button type='button' onClick={() => selectToDo(null)}>cancel</button>
+                                    )
+                                }
+                        </div></li>
+                        </ul>
                     </li>
                 ))
             }  
